ab1af4924bd7895b4a56e1fa57ecb8ff1cf79634
Robin Bastian <robin.bastian@outlook.com>
2022-08-21 21:59:59 +0700

    HKG-6 change credential:customer to 1:1 relationship

---
+++ hasura/migrations/App DB/1661090298436_alter_table_public_customer_alter_column_credential_id/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."customer" drop constraint "customer_credential_id_key";
---
+++ hasura/migrations/App DB/1661090298436_alter_table_public_customer_alter_column_credential_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."customer" add constraint "customer_credential_id_key" unique ("credential_id");
